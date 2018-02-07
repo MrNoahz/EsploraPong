f0529bc1d94c60426eee099494b03e426ad3493b
Noah Bresler <noahbresler2002@gmail.com>
2018-02-07 13:22:53 -0600

    Doing the do's

--- EsploraPong.ino
+++ EsploraPong.ino
@@ -1,9 +1,28 @@
+#include <Esplora.h>
+#include <SPI.h>
 #include <TFT_ST7735.h>
 
-TFT_ST7735 tft = new TFT_ST7735();
+#define CS 7
+#define DC 0
+#define RST 1
+
+TFT_ST7735 tft = TFT_ST7735(CS, DC, RST);
+
+void splash() {
+	tft.clearScreen();
+
+	tft.setTextScale(0);
+	tft.setCursor(CENTER, CENTER);
+	tft.print("Esplora Pong");
+	delay(1000);
+	tft.clearScreen();
+}
 
 void setup() {
+	tft.begin();
+	tft.setRotation(1);
 
+	splash();
 }
 
 void loop() {
